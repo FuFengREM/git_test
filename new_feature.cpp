3bd8573286aadf1a6d4eb445915148bf01bc3ae4
FuFeng_REM <2390224664@qq.com>
2025-12-30 20:44:00 +0800

    在new_feature.cpp中加入了代码

---
+++ new_feature.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"我在main分支中添加了这段话！"<<endl;
+    cout<<"我在main分支中添加了这段话！"<<endl;
+    cout<<"我在main分支中添加了这段话！"<<endl;
+    cout<<"我在main分支中添加了这段话！"<<endl;
+
+    return 0;
+}
